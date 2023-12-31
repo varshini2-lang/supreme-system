b3a5b94ddb6d905681e1c49881e09f5ac7ede909
varshini2-lang <145186068+varshini2-lang@users.noreply.github.com>
2023-12-31 10:05:26 +0530

    main.py

--- main.py
+++ main.py
@@ -1,62 +1,51 @@
 from fastapi import FastAPI, HTTPException
-
-from datetime import datetime
-
 from typing import List
-
-
+import re
+from datetime import datetime
 
 app = FastAPI()
 
+log_file_path = "/path/to/your/access.log"
 
-
-# Replace 'your_access_log_path' with the actual path to your access log file
-
-access_log_path = '/path/to/your/access/log/file.log'
-
-
-
-def parse_log_line(line):
-
-    # Assuming a common log format with date in square brackets
-
-    parts = line.split('[')
-
-    if len(parts) < 2:
-
+def parse_log_entry(entry: str):
+    # Define a regular expression to extract timestamp and other details
+    pattern = r'\[(.*?)\].*?"\S+ (.*?) \S+" (\d+) (\d+)'
+    match = re.match(pattern, entry)
+    
+    if match:
+        timestamp, url, status_code, size = match.groups()
+        return {
+            "timestamp": timestamp,
+            "url": url,
+            "status_code": int(status_code),
+            "size": int(size)
+        }
+    else:
         return None
 
-    timestamp_str = parts[1].split(']')[0]
-
+def read_access_logs(start_time: str, end_time: str) -> List[dict]:
+    logs = []
+    
     try:
-
-        timestamp = datetime.strptime(timestamp_str, '%d/%b/%Y:%H:%M:%S %z')
-
-        return timestamp
-
-    except ValueError:
-
-        return None
-
-
+        with open(log_file_path, "r") as file:
+            for line in file:
+                entry = parse_log_entry(line)
+                if entry:
+                    log_time = datetime.strptime(entry["timestamp"], "%d/%b/%Y:%H:%M:%S %z")
+                    if start_time <= log_time <= end_time:
+                        logs.append(entry)
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="Access log file not found.")
+    
+    return logs
 
 @app.get("/access-logs/")
-
-def get_access_logs(start_time: datetime, end_time: datetime) -> List[str]:
-
+def get_access_logs(start_time: str, end_time: str):
     try:
+        start_time = datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%S%z")
+        end_time = datetime.strptime(end_time, "%Y-%m-%dT%H:%M:%S%z")
+    except ValueError:
+        raise HTTPException(status_code=400, detail="Invalid datetime format. Use ISO 8601 format.")
 
-        with open(access_log_path, 'r') as file:
-
-            logs = [line.strip() for line in file if parse_log_line(line) and start_time <= parse_log_line(line) <= end_time]
-
-            return logs
-
-    except FileNotFoundError:
-
-        raise HTTPException(status_code=404, detail="Access log file not found")
-
-    except Exception as e:
-
-        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
-
+    logs = read_access_logs(start_time, end_time)
+    return {"access_logs": logs}
